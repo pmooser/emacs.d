d96274b7ee226a9f00df48098a808f1c20df3d83
Paul Mooser <pmooser@apple.com>
2023-01-12 16:20:01 +0100

    - adding bqn mode - fixing paredit with newest cider (1.6) - minor config changes (doom modeline)

--- init.el
+++ init.el
@@ -1,21 +1,28 @@
+;;   -*- lexical-binding: t; -*-
 ;; make sure things in my bin directory can be found:
 (defconst bin-dir (concat (getenv "HOME") "/bin"))
 (setenv "PATH" (concat (getenv "PATH") ":" bin-dir))
-(setq exec-path (append exec-path (list bin-dir)))
+
+;; add home/bin and /usr/local/bin to exec-path:
+(setq exec-path (append exec-path (list bin-dir "/usr/local/bin")))
 
 ;; load my elisp directories:
 (defconst elisp-dir (locate-user-emacs-file "elisp"))
 (defconst personal-dir (locate-user-emacs-file "personal"))
+(defconst bqn-dir (locate-user-emacs-file "BQN"))
 
 (load (concat elisp-dir "/subdirectories.el"))
 
 (setq load-path
-     (append (list elisp-dir personal-dir)
+     (append (list elisp-dir personal-dir bqn-dir)
              (subdirectories elisp-dir)
              (subdirectories personal-dir)
              load-path))
 
-;; This is intended for machine-local changes, and thus won't
+;; BQN
+(require 'bqn-mode)
+
+;; This is intended for machine-local changes, and thus won't"))
 ;; actually be checked into the repo.
 (defconst local-settings (locate-user-emacs-file "local.el"))
 (if (file-exists-p local-settings)
@@ -27,6 +34,25 @@
 ;;;;
 ;; general appearance and behavior
 ;;;;
+
+;; mode-line customizationclojure
+
+(setq-default mode-line-format
+              '("%e"
+                mode-line-front-space
+                ;; mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                ;; mode-line-remote
+                ;; mode-line-frame-identification
+                mode-line-buffer-identification
+                "   "
+                mode-line-position
+                ;; (vc-mode vc-mode)
+                "  "
+                mode-line-modes
+                mode-line-misc-info
+                mode-line-end-spaces))
 
 ;; goto-line key
 (global-set-key (kbd "M-g") 'goto-line)
@@ -86,8 +112,17 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(setq package-archives
+     '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+       ("MELPA Stable" . "https://stable.melpa.org/packages/")
+       ("MELPA"        . "https://melpa.org/packages/"))
+     package-archive-priorities
+     '(("MELPA Stable" . 10)
+       ("GNU ELPA"     . 5)
+       ("MELPA"        . 0)))
 
 (package-initialize)
 
@@ -95,7 +130,8 @@
   (dolist (p pairs)
     (add-to-list 'package-pinned-packages p t)))
 
-(pin-packages '(cider . "melpa-stable"))
+;; (pin-packages '(cider . "melpa"))
+(pin-packages '(cider . "MELPA Stable"))
 
 (defun ensure-packages-installed (&rest packages)
   (dolist (p packages)
@@ -107,9 +143,12 @@
   (package-refresh-contents))
 
 (ensure-packages-installed
- 'ac-cider
+ ;; 'ac-cider
  'cider
  'clojure-mode
+ 'lsp-mode
+ 'lsp-treemacs
+ 'flycheck
  'company
  'expand-region
  'idle-highlight-mode
@@ -117,7 +156,15 @@
  'multiple-cursors
  'rainbow-delimiters
  'paredit
- 'undo-tree)
+ 'undo-tree
+ 'ws-butler
+ ;; mode line minor mode menu:
+ 'minions
+ 'doom-modeline
+ 'all-the-icons)
+
+;; version control
+(require 'vc)
 
 ;; expand region
 (require 'expand-region)
@@ -129,6 +176,7 @@
 
 ;; undo tree
 (require 'undo-tree)
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
 (global-undo-tree-mode)
 
 ;; multiple-cursors
@@ -145,6 +193,11 @@
 
 (global-set-key (kbd "C-;") 'toggle-comment-region-or-line)
 
+;; cleanup whitespace of changed lines:
+(require 'ws-butler)
+(add-hook 'clojure-mode-hook #'ws-butler-mode)
+(add-hook 'css-mode-hook #'ws-butler-mode)
+
 ;; default frame characteristics
 (setq default-frame-alist
       '((font . "Menlo-13")
@@ -152,22 +205,125 @@
         (left   . 80)
         (width  . 120)
         (height . 60)
-        (cursor-color . "grey40")
+        (cursor-color . "mediumslateblue")
         (cursor-type . box)
         (foreground-color . "white")
         (background-color . "black")))
 
+;; all-the-icons
+(require 'all-the-icons)
+
+;; enable minions mode
+(minions-mode 1)
+
+;; doom modeline
+(doom-modeline-mode 1)
+;;(setq doom-modeline-buffer-file-name-style 'auto)
+;; (setq doom-modeline-icon t)
+;; (setq doom-modeline-major-mode-icon t)
+;; (setq doom-modeline-major-mode-color-icon t)
+(setq doom-modeline-minor-modes t)
+(setq doom-modeline-buffer-encoding nil)
+
+;; (set-face-background 'modeline "blue")
+
+;; (set-face-attribute 'mode-line nil
+;;                     :background "#353644"
+;;                     :foreground "white"
+;;                     :box '(:line-width 1 :color "#353644")
+;;                     :overline nil
+;;                     :underline nil)
+
+;;;;
+;; CIDER
+;;;;
+
+(setq cider-default-cljs-repl 'shadow)
+
+;; always do a top/bottom split
+(setq
+ split-width-threshold nil
+ split-height-threshold 0)
+
+(defun get-string-from-file (filePath)
+  "Return filePath's file content."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+;; NOTE - cider gets confused sometimes, so we might have to occasionally explicitly call:
+;;   cider-set-repl-type
+
+(defun clj-connect (project-name)
+  "Connect to nREPL for `project-name`."
+  (interactive "sProject name: ")
+  (let ((project-dir (concat "/Users/pmooser/work/projects/" project-name)))
+    (when (not (file-directory-p project-dir))
+      (error "No such project directory: %s" project-dir))
+    (let* ((nrepl-port (string-to-number (get-string-from-file (concat project-dir "/.nrepl-port"))))
+           (params (list :host "localhost" :port nrepl-port :project-dir project-dir)))
+      (cider-connect-clj params))))
+
+(defun shadow-connect (project-name)
+  "Connect to shadow-cljs nREPL for `project-name`. Assumes we already have another nREPL connection."
+  (interactive "sProject name: ")
+  (let ((project-dir (concat "/Users/pmooser/work/projects/" project-name)))
+    (when (not (file-directory-p project-dir))
+      (error "No such project directory: %s" project-dir))
+    (let* ((nrepl-port (string-to-number (get-string-from-file (concat project-dir "/.shadow-cljs/nrepl.port"))))
+           (params (list :host "localhost" :port nrepl-port :cljs-repl-type 'shadow)))
+      (if (null (cider-repls))
+        (cider-connect-cljs params)
+        (cider-connect-sibling-cljs params)))))
+
+;;;;
+;; LSP
+;;;;
+
+;; (add-hook 'clojure-mode-hook 'lsp)
+;; (add-hook 'clojurescript-mode-hook 'lsp)
+;; (add-hook 'clojurec-mode-hook 'lsp)
+
+;; (setq gc-cons-threshold (* 100 1024 1024)
+;;       read-process-output-max (* 1024 1024)
+;;       treemacs-space-between-root-nodes nil
+;;       company-minimum-prefix-length 1
+;;       ;; header at top of buffer:
+;;       lsp-headerline-breadcrumb-enable nil
+;;       lsp-lens-enable nil
+;;       lsp-signature-auto-activate nil
+;;       ;; lsp-enable-indentation nil ;; uncomment to use cider indentation instead of lsp
+;;       ;; lsp-enable-completion-at-point nil ;; uncomment to use cider completion instead of lsp
+;;       )
+
+;;;;
+;; faces
+;;;;
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-result-overlay-face ((t (:background "dark cyan" :box (:line-width (2 . -1) :color "MediumPurple3")))))
  '(cider-stacktrace-face ((t (:inherit default :foreground "SkyBlue1"))))
+ '(company-preview-common ((t (:inherit default :foreground "white" :background "light slate gray"))))
+ '(company-scrollbar-bg ((t (:background "#ffffffffffff"))))
+ '(company-scrollbar-fg ((t (:background "#ffffffffffff"))))
+ '(company-tooltip ((t (:inherit default :background "#ffffffffffff"))))
+ '(company-tooltip-common ((t (:inherit default :foreground "white" :background "#ffffffffffff"))))
+ '(company-tooltip-common-selection ((t (:inherit default :foreground "grey" :background "#ffffffffffff"))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face :background "#ffffffffffff"))))
  '(diff-added ((t (:inherit diff-changed :background "LightGreen" :foreground "black"))))
  '(diff-indicator-added ((t (:inherit diff-added :background "black" :foreground "LightGreen"))))
  '(diff-indicator-removed ((t (:inherit diff-removed :background "black" :foreground "LightPink"))))
  '(diff-removed ((t (:inherit diff-changed :background "pink" :foreground "black"))))
+ '(doom-modeline-bar ((t (:inherit doom-modeline-highlight :background "DeepSkyBlue1"))))
+ '(doom-modeline-buffer-minor-mode ((t (:inherit font-lock-doc-face :foreground "grey" :slant normal))))
+ '(doom-modeline-buffer-modified ((t (:inherit (error bold) :foreground "orange"))))
+ '(doom-modeline-project-dir ((t (:inherit (font-lock-string-face bold) :foreground "LightSkyBlue"))))
  '(idle-highlight ((t (:inherit region :background "grey25"))))
+ '(mode-line ((t (:background "gray26" :foreground "gray90" :box (:line-width (1 . -1) :style released-button)))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "grey30" :foreground "gray70" :box (:line-width -1 :color "grey40") :weight light))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#FFAAAA"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#FFAA55"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#FFFFAA"))))
@@ -183,5 +339,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-encoding nil)
+ '(doom-modeline-minor-modes t)
+ '(doom-modeline-mode t)
  '(ido-enable-flex-matching t)
- '(ns-antialias-text nil))
+ '(minions-mode-line-lighter "⚙")
+ '(ns-antialias-text t)
+ '(package-selected-packages
+   '(all-the-icons doom-modeline minions ws-butler flycheck lsp-treemacs lsp-mode undo-tree paredit rainbow-delimiters multiple-cursors magit idle-highlight-mode expand-region company cider ac-cider)))
+
+;; scroll left/right using trackpad gestures
+(setq mouse-wheel-tilt-scroll t)
+(setq mouse-wheel-flip-direction t)
--- personal/paul-clojure.el
+++ personal/paul-clojure.el
@@ -10,9 +10,6 @@
 
 (setq cider-stacktrace-frames-background-color "black")
 
-;; hack to enable font-lock in cider repl (https://github.com/clojure-emacs/cider/issues/749)
-(add-hook 'cider-repl-mode-hook 'clojure-font-lock-setup)
-
 ;; For some reason these 4 lines are critical to make
 ;; paredit work properly in the REPL - otherwise it will
 ;; not backspace properly over {} or []:
@@ -36,43 +33,23 @@
 
 ;; END non-standard paredit customizations
 
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
+(autoload 'enable-paredit-mode "paredit" "Minor mode for pseudo-structurally editing Lisp code." t)
 
 ;; setup for lisps in general
 
 (defvar lisp-modes '(emacs-lisp-mode-hook
-                     clojure-mode-hook
-                     cider-mode-hook))
+                     clojure-mode-hook))
 
 (defun do-lisps-setup ()
   (rainbow-delimiters-mode t)
   (idle-highlight-mode t)
   (show-paren-mode t)
-  (paredit-mode +1))
+  (enable-paredit-mode))
 
 (dolist (hook lisp-modes)
   (add-hook hook 'do-lisps-setup))
 
-(add-hook 'cider-mode-hook 'eldoc-mode)
-
-;;;;
-;; ac-cider
-;;;;
-
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-
-;; (require 'ac-cider)
-
-;; (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-;; (add-hook 'cider-mode-hook 'ac-cider-setup)
-;; (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-
-;; (eval-after-load "auto-complete"
-;;   '(progn
-;;      (add-to-list 'ac-modes 'cider-mode)
-;;      (add-to-list 'ac-modes 'cider-repl-mode)))
+;; (add-hook 'cider-mode-hook 'eldoc-mode)
 
 ;;;;
 ;; company mode
@@ -99,10 +76,11 @@
 (require 'company)
 
 (global-company-mode)
+;; (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
 
-(add-hook 'company-mode-hook 'company-colors)
-(add-hook 'cider-mode-hook 'company-mode)
-(add-hook 'cider-repl-mode-hook 'company-mode)
+(add-hook 'company-mode-hook #'company-colors)
+(add-hook 'cider-mode-hook #'company-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
 
 ;;;;
 ;; cider repl
@@ -113,7 +91,16 @@
 (add-hook 'cider-repl-mode-hook
           (lambda ()
             (show-paren-mode t)
-            (paredit-mode +1)))
+            (enable-paredit-mode)
+            ;; since CIDER 1.6.0, for some reason paredit's binding of RET
+            ;;   is preventing the repl from working, so only for cider-repl-mode,
+            ;;   override the paredit behavior:
+            (let ((old-map (cdr (assoc 'paredit-mode minor-mode-map-alist)))
+                  (new-map (make-sparse-keymap)))
+              (set-keymap-parent new-map old-map)
+              (define-key new-map (kbd "RET") nil)
+              (make-local-variable 'minor-mode-overriding-map-alist)
+              (push `(paredit-mode . ,new-map) minor-mode-overriding-map-alist))))
 
 ;; formatting
 
